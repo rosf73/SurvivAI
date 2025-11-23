15e03634a4798b342e3d5daebca4cf60f64394b7
rosf73 <alswlzhsclq@naver.com>
2025-11-23 20:57:54 +0900

    [feat] Update version format

--- composeApp/src/commonMain/kotlin/com/survivai/survivai/config/BuildConfig.kt
+++ composeApp/src/commonMain/kotlin/com/survivai/survivai/config/BuildConfig.kt
@@ -4,10 +4,9 @@ package com.survivai.survivai.config
  * 앱 빌드 정보
  */
 object BuildConfig {
-    const val VERSION_NAME = "1.0.0"
-    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+    const val VERSION_CODE = 10
 
     // 선택사항: 추가 정보
-    const val BUILD_TYPE = "debug" // "debug" or "release"
     const val APP_NAME = "SurvivAI"
 }

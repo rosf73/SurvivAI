1c152b39d5af2da0bd1719fb80523dcc391876ff
rosf73 <alswlzhsclq@naver.com>
2025-12-07 18:11:13 +0900

    [feat/#25] Add drawImage function to Canvas

--- composeApp/src/commonMain/kotlin/com/survivai/survivai/game/colosseum/Canvas.kt
+++ composeApp/src/commonMain/kotlin/com/survivai/survivai/game/colosseum/Canvas.kt
@@ -1,10 +1,18 @@
 package com.survivai.survivai.game.colosseum
 
+import androidx.annotation.FloatRange
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Offset.Companion
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultBlendMode
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.text.TextMeasurer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -15,6 +23,7 @@ interface GameDrawScope {
     fun drawRect(color: Color, topLeft: Offset, width: Float, height: Float)
     fun drawText(textMeasurer: TextMeasurer, text: String, topLeft: Offset, size: Size = Size.Unspecified, style: TextStyle = TextStyle.Default, softWrap: Boolean = true)
     fun drawPath(path: Path, color: Color)
+    fun drawImage(image: ImageBitmap, topLeft: Offset = Offset.Zero, @FloatRange(from = 0.0, to = 1.0) alpha: Float = 1.0f, style: DrawStyle = Fill, colorFilter: ColorFilter? = null)
 }
 
 interface Canvas {

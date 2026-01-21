60c29bd0744b81cf8ac7336ae02a0bba0b37c469
rosf73 <alswlzhsclq@naver.com>
2026-01-21 15:44:08 +0900

    [feat/#29] Add player factory
    Signed-off-by: rosf73 <alswlzhsclq@naver.com>

---
+++ composeApp/src/commonMain/kotlin/com/survivai/survivai/game/colosseum/entity/ColosseumPlayerFactory.kt
@@ -0,0 +1,44 @@
+package com.survivai.survivai.game.colosseum.entity
+
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import com.survivai.survivai.game.sprite.AnimationAction
+import com.survivai.survivai.game.sprite.SpriteAnimationData
+import com.survivai.survivai.game.sprite.SpriteLoader
+import com.survivai.survivai.game.sprite.SpriteSheet
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+
+class ColosseumPlayerFactory(
+    private val loader: SpriteLoader,
+) {
+    suspend fun createPlayer(
+        name: String,
+        radius: Float,
+        color: Color,
+        startHp: Int,
+        ripIcons: Pair<ImageBitmap, ImageBitmap>,
+    ): ColosseumPlayer = coroutineScope {
+        val attackAnimation = async {
+            loader.load(
+                "sprite_colosseum_attack_right.png", // temporary
+                SpriteAnimationData.sequenced(
+                    totalFrame = 3,
+                    durationPerFrame = 1.2,
+                    frameSize = Size(1),
+                    loop = false,
+                    nextAction = AnimationAction.IDLE,
+                ),
+            )
+        }
+        val sheet = SpriteSheet(mapOf(
+//            AnimationAction.IDLE to ...,
+            AnimationAction.ATTACK to attackAnimation.await(),
+        ))
+
+        ColosseumPlayer(
+            name, radius, color, startHp, ripIcons, sheet
+        )
+    }
+}

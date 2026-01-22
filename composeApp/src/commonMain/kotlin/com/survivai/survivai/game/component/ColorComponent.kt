5ed12c11faff71867d1b4fcf65ba80b0f05f6b87
rosf73 <alswlzhsclq@naver.com>
2026-01-22 15:27:49 +0900

    [feat/#29] Update tintColor to constructor parameter
    Signed-off-by: rosf73 <alswlzhsclq@naver.com>

--- composeApp/src/commonMain/kotlin/com/survivai/survivai/game/component/ColorComponent.kt
+++ composeApp/src/commonMain/kotlin/com/survivai/survivai/game/component/ColorComponent.kt
@@ -4,8 +4,9 @@ import androidx.compose.ui.graphics.Color
 import com.survivai.survivai.game.Entity
 import com.survivai.survivai.game.World
 
-class ColorComponent : Component() {
-    var tintColor: Color? = null
+class ColorComponent(
+    var tintColor: Color? = null,
+) : Component() {
 
     override fun update(deltaTime: Double, owner: Entity, world: World) {
         // TODO : update tintColor according to changed status

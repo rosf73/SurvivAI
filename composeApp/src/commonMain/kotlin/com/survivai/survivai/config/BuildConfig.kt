cda1eae45a0fe54f7783cc8a5d1e253229a67adb
rosf73 <alswlzhsclq@naver.com>
2026-01-28 16:19:57 +0900

    [release/1.2] Update version to v1.2
    Signed-off-by: rosf73 <alswlzhsclq@naver.com>

--- composeApp/src/commonMain/kotlin/com/survivai/survivai/config/BuildConfig.kt
+++ composeApp/src/commonMain/kotlin/com/survivai/survivai/config/BuildConfig.kt
@@ -4,10 +4,10 @@ package com.survivai.survivai.config
  * 앱 빌드 정보
  */
 object BuildConfig {
-    const val VERSION_NAME = "1.1"
-    const val VERSION_CODE = 11
+    const val VERSION_NAME = "1.2"
+    const val VERSION_CODE = 12
 
     // 선택사항: 추가 정보
     const val APP_NAME = "SurvivAI"
-    const val DEBUG = true
+    const val DEBUG = false
 }

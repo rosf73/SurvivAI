74dde3b1cedd8d91c93b99df2842e0fb1b76f950
rosf73 <alswlzhsclq@naver.com>
2026-02-13 10:42:42 +0900

    [feat/#59] Refactor dashboard composable in end screen to public composable
    Signed-off-by: rosf73 <alswlzhsclq@naver.com>

--- composeApp/src/commonMain/kotlin/com/survivai/survivai/game/colosseum/components/ColosseumEndScreen.kt
+++ composeApp/src/commonMain/kotlin/com/survivai/survivai/game/colosseum/components/ColosseumEndScreen.kt
@@ -3,7 +3,6 @@ package com.survivai.survivai.game.colosseum.components
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,31 +11,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.survivai.survivai.common.squareVerticalScrollbar
@@ -201,7 +194,7 @@ private fun ResultArea(
         Row(
             modifier = modifier,
         ) {
-            Dashboard(
+            ColosseumScoreboard(
                 statsList = statsList,
                 modifier = Modifier
                     .weight(7f)
@@ -221,7 +214,7 @@ private fun ResultArea(
         Column(
             modifier = modifier,
         ) {
-            Dashboard(
+            ColosseumScoreboard(
                 statsList = statsList,
                 modifier = Modifier
                     .fillMaxWidth()
@@ -406,63 +399,6 @@ private fun TitlesFixedCard(
 
                 // 칭호 간 간격
                 Spacer(modifier = Modifier.size(16.dp))
-            }
-        }
-    }
-}
-
-@Composable
-private fun Dashboard(
-    statsList: List<List<StatCell>>,
-    modifier: Modifier = Modifier,
-) {
-    Card(
-        modifier = modifier
-            .border(
-                BorderStroke(3.dp, Brush.verticalGradient(listOf(Color(0xFF00E5FF), Color(0xFF00838F)))),
-            ),
-        colors = CardDefaults.cardColors(containerColor = Color(0x00FFFFFF)),
-    ) {
-        LazyColumn {
-            itemsIndexed(statsList) { rowIndex, rowData ->
-                val backgroundColor = when (rowIndex) {
-                    0 -> Color(0xFF4FC3F7) // title color
-                    else -> Color(0x00FFFFFF)
-                }
-
-                Row(
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    rowData.forEachIndexed { colIndex, cellText ->
-                        val cellModifier = if (colIndex == 0) {
-                            Modifier.width(120.dp)
-                        } else {
-                            // same width
-                            Modifier.weight(1f)
-                        }
-
-                        Box(
-                            modifier = cellModifier
-                                .background(backgroundColor)
-                                .border(
-                                    width = 1.dp,
-                                    color = Color(0xFFCCCCCC),
-                                )
-                                .padding(8.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(
-                                text = cellText.stat,
-                                style = LocalTextStyle.current.copy(
-                                    fontSize = 12.sp,
-                                    textAlign = TextAlign.Center,
-                                    fontWeight = cellText.weight,
-                                ),
-                                color = cellText.color,
-                            )
-                        }
-                    }
-                }
             }
         }
     }
